1953498e313e943d1c22d7050c9424ff739c9415
skybob22 <skybob22@users.noreply.github.com>
2020-09-16 00:14:28 -0700

    Coppied assignment 2 as starting point for assignment 3

---
+++ Assignment_3/Source_Files/include/frontend/Source.h
@@ -0,0 +1,114 @@
+/**
+ * Source class for a simple interpreter.
+ *
+ * (c) 2020 by Ronald Mak
+ * Department of Computer Science
+ * San Jose State University
+ */
+#ifndef SOURCE_H_
+#define SOURCE_H_
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+namespace frontend {
+
+using namespace std;
+
+class Source
+{
+private:
+    ifstream source;
+    string sourceFileName;
+    int  lineNum;      // current source line number
+    char currentCh;    // current source character
+
+    //Used for 'peeking' at next character without consuming it
+    bool hold;
+    char heldCh;
+
+public:
+    static const char EOL = '\n';
+
+    /**
+     * Constructor
+     * @param sourceFileName the source file name.
+     */
+    Source(string sourceFileName) : lineNum(1),hold(false),heldCh(0)
+    {
+        source.open(sourceFileName);
+
+        if (source.fail())
+        {
+            cout << "*** ERROR: Failed to open " << sourceFileName << endl;
+            exit(-1);
+        }
+
+        currentCh = nextChar();  // read the first character of the file
+    }
+
+    /**
+     * Getter.
+     * @return the current source line number.
+     */
+    int lineNumber() const { return lineNum; }
+
+    /**
+     * Getter.
+     * @return the current source character.
+     */
+    char currentChar() const { return currentCh; }
+
+    /**
+     * Read and return the next input source character.
+     * @return the character, or EOF if at the end of the file.
+     */
+    char nextChar()
+    {
+        if(hold)
+        {
+            //There is a 'held' character, replace read with load
+            hold = false;
+            currentCh = heldCh;
+
+            if(heldCh == EOL){
+                //Account for 'peeking' possibly incrementing newlines
+                lineNum--;
+            }
+        }
+        else{
+            //Get next character normally
+            currentCh = source.get();
+        }
+
+        if (source.eof()) currentCh = EOF;
+        else if (currentCh == EOL) lineNum++;
+
+        else if (source.fail())
+        {
+            cout << "*** ERROR: Failed to read " << sourceFileName << endl;
+            exit(-1);
+        }
+
+        return currentCh;
+    }
+
+    /**
+     * Read the next input character without consuming it,
+     * such that the next call to 'nextChar' will still return it
+     * @return
+     */
+    char peek()
+    {
+        char nextCh = nextChar();
+        hold = true;
+        heldCh = nextCh;
+
+        return nextCh;
+    }
+};
+
+}  // namespace frontend
+
+#endif /* SOURCE_H_ */
---
+++ Assignment_3/Source_Files/src/frontend/Token.cpp
@@ -0,0 +1,286 @@
+/**
+ * Token class for a simple interpreter.
+ *
+ * (c) 2020 by Ronald Mak
+ * Department of Computer Science
+ * San Jose State University
+ */
+#include <string>
+#include <map>
+#include <ctype.h>
+
+#include "Object.h"
+#include "frontend/Token.h"
+
+namespace frontend {
+
+using namespace std;
+
+map<string, TokenType> Token::reservedWords;
+
+void Token::initialize()
+{
+    reservedWords["PROGRAM"]    = PROGRAM;
+    reservedWords["BEGIN"]      = BEGIN;
+    reservedWords["END"]        = END;
+    reservedWords["REPEAT"]     = REPEAT;
+    reservedWords["UNTIL"]      = UNTIL;
+    reservedWords["WRITE"]      = WRITE;
+    reservedWords["WRITELN"]    = WRITELN;
+
+    //Newly added tokens
+    reservedWords["DIV"]        = DIV;
+    reservedWords["MOD"]        = MOD;
+    reservedWords["AND"]        = AND;
+    reservedWords["OR"]         = OR;
+    reservedWords["NOT"]        = NOT;
+    reservedWords["CONST"]      = CONST;
+    reservedWords["TYPE"]       = TYPE;
+    reservedWords["VAR"]        = VAR;
+    reservedWords["PROCEDURE"]  = PROCEDURE;
+    reservedWords["FUNCTION"]   = FUNCTION;
+    reservedWords["WHILE"]      = WHILE;
+    reservedWords["DO"]         = DO;
+    reservedWords["FOR"]        = FOR;
+    reservedWords["TO"]         = TO;
+    reservedWords["DOWNTO"]     = DOWNTO;
+    reservedWords["IF"]         = IF;
+    reservedWords["THEN"]       = THEN;
+    reservedWords["ELSE"]       = ELSE;
+    reservedWords["CASE"]       = CASE;
+    reservedWords["OF"]         = OF;
+}
+
+Token *Token::Word(char firstChar, Source *source)
+{
+    Token *token = new Token(firstChar);
+    token->lineNumber = source->lineNumber();
+
+    // Loop to get the rest of the characters of the word token.
+    // Append letters and digits to the token.
+    for (char ch = source->nextChar(); isalnum(ch); ch = source->nextChar())
+    {
+        token->text += ch;
+    }
+
+    // Is it a reserved word or an identifier?
+    string upper = toUpperCase(token->text);
+    if (Token::reservedWords.find(upper) != Token::reservedWords.end())
+    {
+        token->type = Token::reservedWords[upper];
+    }
+    else
+    {
+        token->type = TokenType::IDENTIFIER;
+    }
+
+    return token;
+}
+
+Token *Token::Number(char firstChar, Source *source)
+{
+    Token *token = new Token(firstChar);
+    token->lineNumber = source->lineNumber();
+    int pointCount = 0;
+
+    // Loop to get the rest of the characters of the number token.
+    // Append digits to the token.
+    for (char ch = source->nextChar();
+         isdigit(ch) || (ch == '.');
+         ch = source->nextChar())
+    {
+        if (ch == '.') pointCount++;
+        token->text += ch;
+    }
+
+    // Integer constant.
+    if (pointCount == 0)
+    {
+        token->type    = TokenType::INTEGER;
+        token->value.L = stol(token->text);
+        token->value.D = token->value.L;  // allow using integer value as double
+    }
+
+    // Real constant.
+    else if (pointCount == 1)
+    {
+        token->type    = TokenType::REAL;
+        token->value.D = stod(token->text);
+    }
+
+    else tokenError(token, "Invalid number");
+
+    return token;
+}
+
+Token *Token::String(char firstChar, Source *source)
+{
+    Token *token = new Token(firstChar);  // the leading '
+    token->lineNumber = source->lineNumber();
+
+    // Loop to append the rest of the characters of the string,
+    bool EOS = false; //End-of-String
+    for (char ch = source->nextChar();!EOS; ch = source->nextChar())
+    {
+        //Check if we've reached the end of the file
+        if(ch == EOF)
+        {
+            //Reached end of file
+            tokenError(token, "String not closed");
+            token->type = TokenType::ERROR;
+            return token;
+        }
+        else if(ch == '\'')
+        {
+            //Check if it is the end of string or literal quote
+            char nextChar = source->nextChar();
+            if(nextChar != '\'')
+            {
+                //End of string
+                EOS = true;
+                token->text += ch;
+                break;
+            }
+        }
+
+        token->text += ch;
+    }
+
+    token->type = TokenType::STRING;
+
+    // Don't include the leading and trailing '.
+    token->value.S = token->text.substr(1, token->text.length() - 2);
+
+    return token;
+}
+
+Token *Token::SpecialSymbol(char firstChar, Source *source)
+{
+    Token *token = new Token(firstChar);
+    token->lineNumber = source->lineNumber();
+
+    switch (firstChar)
+    {
+
+        case ';' : token->type = TokenType::SEMICOLON;  break;
+        case '+' : token->type = TokenType::PLUS;       break;
+        case '-' : token->type = TokenType::MINUS;      break;
+        case '*' : token->type = TokenType::STAR;       break;
+        case '/' : token->type = TokenType::SLASH;      break;
+        case '=' : token->type = TokenType::EQUALS;     break;
+        case '(' : token->type = TokenType::LPAREN;     break;
+        case ')' : token->type = TokenType::RPAREN;     break;
+
+        //Newly added
+        case ',' : token->type = TokenType::COMMA;      break;
+        case '[' : token->type = TokenType::LBRACKET;   break;
+        case ']' : token->type = TokenType::RBRACKET;   break;
+        case '^' : token->type = TokenType::CARAT;     break;
+
+        case '.' :
+        {
+            char nextChar = source->peek();
+
+            //Is it the .. symbol?
+            if (nextChar == '.')
+            {
+                token->text += source->nextChar();
+                token->type = TokenType::DOT_DOT;
+            }
+
+            // It's just the . symbol
+            else
+            {
+                token->type = TokenType::PERIOD;
+            }
+
+            break;
+        }
+
+        case '<' :
+        {
+            char nextChar = source->peek();
+
+            // Is it the <> symbol?
+            if (nextChar == '>')
+            {
+                token->text += source->nextChar();
+                token->type = TokenType::NOT_EQUALS;
+            }
+
+            // Is it the <= symbol?
+            else if (nextChar == '=')
+            {
+                token->text += source->nextChar();
+                token->type = TokenType::LESS_EQUALS;
+            }
+
+            // It's just the < symbol
+            else
+            {
+                token->type = TokenType::LESS_THAN;
+            }
+
+            break;
+        }
+
+        case '>' :
+        {
+            char nextChar = source->peek();
+
+            // Is it the >= symbol?
+            if (nextChar == '=')
+            {
+                token->text += source->nextChar();
+                token->type = TokenType::GREATER_EQUALS;
+            }
+
+            // It's just the > symbol
+            else
+            {
+                token->type = TokenType::GREATER_THAN;
+            }
+
+            break;
+        }
+
+        case ':' :
+        {
+            char nextChar = source->peek();
+
+            // Is it the := symbol?
+            if (nextChar == '=')
+            {
+                token->text += source->nextChar();
+                token->type = TokenType::COLON_EQUALS;
+            }
+
+            // No, it's just the : symbol.
+            else
+            {
+                token->type = TokenType::COLON;
+                return token;  // already consumed :
+            }
+
+            break;
+        }
+
+        case EOF : token->type = END_OF_FILE; break;
+
+        default:{
+            tokenError(token, "Invalid Token");
+            token->type = TokenType::ERROR;
+        }
+    }
+
+    source->nextChar();  // consume the special symbol
+    return token;
+}
+
+void Token::tokenError(Token *token, string message)
+{
+    printf("TOKEN ERROR at line %d: %s at '%s'\n",
+           token->lineNumber, message.c_str(), token->text.c_str());
+}
+
+}  // namespace frontend

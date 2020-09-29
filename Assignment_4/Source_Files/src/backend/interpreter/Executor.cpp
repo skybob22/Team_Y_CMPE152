8094ec4e78574a9cc8498a43b35e8c01fb6f298d
= <steven.west@sjsu.edu>
2020-09-28 17:31:31 -0700

    Added While and For loops + If and Case statements

--- Assignment_4/Source_Files/src/backend/interpreter/Executor.cpp
+++ Assignment_4/Source_Files/src/backend/interpreter/Executor.cpp
@@ -9,21 +9,17 @@ using namespace std;
 
 Object Executor::visitProgram(Pcl4Parser::ProgramContext *ctx)
 {
-    cout << "Visiting program" << endl;
 
     return visit(ctx->block());
 }
 
 Object Executor::visitStatement(Pcl4Parser::StatementContext *ctx)
 {
-    cout << "Line " << ctx->getStart()->getLine() << ": ";
     return visitChildren(ctx);
 }
 
 Object Executor::visitStatementList(Pcl4Parser::StatementListContext *ctx)
 {
-    cout << "Visiting statement list" << endl;
-
     for (Pcl4Parser::StatementContext *stmtCtx : ctx->statement())
     {
         visit(stmtCtx);
@@ -34,63 +30,163 @@ Object Executor::visitStatementList(Pcl4Parser::StatementListContext *ctx)
 
 Object Executor::visitCompoundStatement(Pcl4Parser::CompoundStatementContext *ctx)
 {
-    cout << "Visiting compound statement" << endl;
     return visit(ctx->statementList());
 }
 
 Object Executor::visitAssignmentStatement(Pcl4Parser::AssignmentStatementContext *ctx)
 {
-    cout << "Visiting assignment statement" << endl;
     string variableName = ctx->lhs()->variable()->getText();
     visit(ctx->lhs());
     Object value = visit(ctx->rhs());
 
-    cout << "Will assign value " << value.as<int>()
-         << " to variable " << variableName << endl;
+    intermediate::symtab::SymtabEntry* entry = symbol_table.enter(variableName);
+    if(value.is<int>()){
+        entry->setValue(value.as<int>());
+    }
+    else{
+        entry->setValue(value);
+    }
     return nullptr;
 }
 
 Object Executor::visitRepeatStatement(Pcl4Parser::RepeatStatementContext *ctx)
 {
-    cout << "Visiting REPEAT statement" << endl;
-    return visit(ctx->statementList());
+    do{
+        visitStatementList(ctx->statementList());
+    } while(visitExpression(ctx->expression()).equals(false));
+
+   return nullptr;
 }
+
+Object Executor::visitWhileLoop(Pcl4Parser::WhileLoopContext *ctx) {
+    while(visitExpression(ctx->expression()).equals(false)){
+        visitStatement(ctx->statement());
+    }
+    return nullptr;
+}
+
+Object Executor::visitForLoop(Pcl4Parser::ForLoopContext *ctx) {
+    //Figure out whether we are incrementing or decrementing the counter
+    std::function<antlrcpp::Any(antlrcpp::Any)> op;
+    std::function<bool(antlrcpp::Any,antlrcpp::Any)> comp;
+    if(ctx->TO()){
+        //Increment the counter
+        op = [](antlrcpp::Any c){
+            volatile int d = c.as<int>();
+            return antlrcpp::Any(c.as<int>()+1);
+        };
+        comp = [](antlrcpp::Any c,antlrcpp::Any stop){
+            return c.as<int>() <= stop.as<int>();
+        };
+    }
+    else{
+        //Decrement the counter
+        op = [](antlrcpp::Any c){
+            return antlrcpp::Any(c.as<int>()-1);
+        };
+        comp = [](antlrcpp::Any c,antlrcpp::Any stop){
+            return c.as<int>() >= stop.as<int>();
+        };
+    }
+
+    for(antlrcpp::Any i = visitExpression(ctx->expression(0));comp(i,visitExpression(ctx->expression(0)));i = op(i)){
+        visitStatement(ctx->statement());
+    }
+
+    return nullptr;
+}
+
+Object Executor::visitIfStatement(Pcl4Parser::IfStatementContext *ctx){
+    if(visitExpression(ctx->expression()).equals(true)){
+        visitStatement(ctx->statement(0));
+    }
+    else if(ctx->children.size() > 4){
+        //Visit the else statement if it exists and the expression is not true
+        visitStatement(ctx->statement(1));
+    }
+    return nullptr;
+}
+
+Object Executor::visitCaseStatement(Pcl4Parser::CaseStatementContext *ctx){
+    cout << "Visiting Case" << endl << endl;
+    int value = visitExpression(ctx->expression()).as<int>();
+
+    bool done = false;
+    for(int i=0;i<ctx->children.size() && !done;i++){
+
+        std::set<int> constants = visitConstantList(ctx->caseBlock(i)->constantList()).as<std::set<int>>();
+        if(constants.find(value) != constants.end()){
+            visitCaseBlock(ctx->caseBlock(i));
+            done = true;
+        }
+    }
+    return nullptr;
+}
+
+Object Executor::visitCaseBlock(Pcl4Parser::CaseBlockContext *ctx){
+    //Called after checking the constants
+    visitStatement(ctx->statement());
+    return nullptr;
+}
+
+Object Executor::visitConstantList(Pcl4Parser::ConstantListContext *ctx){
+    std::set<int> constants;
+    for(int i=0;i<ctx->children.size();i++){
+        constants.insert(visitIntegerConstant(ctx->integerConstant(i)).as<int>());
+    }
+    return constants;
+}
+
 
 Object Executor::visitWriteStatement(Pcl4Parser::WriteStatementContext *ctx)
 {
-    cout << "Visiting WRITE statement" << endl;
+    cout << ctx->toString();
     return nullptr;
 }
 
 Object Executor::visitWritelnStatement(Pcl4Parser::WritelnStatementContext *ctx)
 {
-    cout << "Visiting WRITELN statement" << endl;
+    cout << ctx->toString() << endl;
     return nullptr;
 }
 
 Object Executor::visitExpression(Pcl4Parser::ExpressionContext *ctx)
 {
-    cout << "Visiting expression" << endl;
+    //TODO: This isn't working, need to fix visitExpression
     return visitChildren(ctx);
 }
 
 Object Executor::visitVariable(Pcl4Parser::VariableContext *ctx)
 {
-    cout << "Visiting variable ";
+    //cout << "Visiting variable ";
     string variableName = ctx->getText();
     cout << variableName << endl;
 
-    return nullptr;  // should return the variable's value!
+    intermediate::symtab::SymtabEntry* entry = symbol_table.lookup(variableName);
+
+    if(entry != nullptr){
+        //cout << "stored value: " << entry->getValue() << endl;
+        return entry->getValue();
+    }
+    else{
+        return nullptr;
+    }
 }
 
 Object Executor::visitNumber(Pcl4Parser::NumberContext *ctx)
 {
-    cout << "Visiting number: got value ";
+    //cout << "Visiting number: got value ";
     string text = ctx->unsignedNumber()->integerConstant()
                                        ->INTEGER()->getText();
     int value = stoi(text);
     cout << value << endl;
 
+    return value;
+}
+
+Object Executor::visitIntegerConstant(Pcl4Parser::IntegerConstantContext *ctx){
+    string text = ctx->INTEGER()->toString();
+    int value = stoi(text);
     return value;
 }
 

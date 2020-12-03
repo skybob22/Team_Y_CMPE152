c9809d77b51def4662ef4529e4b31195882da29c
skybob22 <skybob22@users.noreply.github.com>
2020-12-03 03:11:25 -0800

    Undid some changes to assignment 6, that I made while messing around

--- Assignment_6/Source_Files/src/backend/interpreter/Executor.cpp
+++ Assignment_6/Source_Files/src/backend/interpreter/Executor.cpp
@@ -81,23 +81,11 @@ void Executor::assignValue(Cell *targetCell, Typespec *targetType,
     }
     else if (targetType == Predefined::realType)
     {
-        try {
-            double doubleValue =
-                    (valueType == Predefined::integerType) ? value.as<int>()
-                                                           : (valueType == Predefined::charType) ? value.as<char>()
-                                                                                                 : value.as<double>();
-            targetCell->setValue(doubleValue);
-        }
-        catch (std::bad_cast z) {
-            cout << z.what() << endl;
-            bool a = value.is<int>();
-            bool b = value.is<float>();
-            bool c = value.is<long>();
-            bool d = value.is<double>();
-            bool e = value.is<bool>();
-            bool f = value.is<int>();
-            int j = 0;
-        }
+        double doubleValue =
+                (valueType == Predefined::integerType) ? value.as<int>()
+                                                       : (valueType == Predefined::charType) ? value.as<char>()
+                                                                                             : value.as<double>();
+        targetCell->setValue(doubleValue);
     }
     else if (targetType == Predefined::stringType)
     {
